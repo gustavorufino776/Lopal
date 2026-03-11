0f917c536bc33f24978bc8699ca32c4d7a79051c
gustavorufino776 <gustavorufino776@gmail.com>
2026-03-11 16:43:42 -0300

    feat: Calculadora terminada e iniciada Urna Eletrônica

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -36,8 +36,8 @@ function calculadora(){
                     return
                 }
         }
-        if((operador != "/" || operando2 != 0)){
-            alert("resultado: " +
+          if( (operador !="/" || operando2 != 0) && (operador_valido != false) ){
+             alert("Resultado: " +
             operando1 + " " + operador + " " + operando2 + " = " + resultado);
         }
         
